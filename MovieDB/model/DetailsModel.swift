930cf05fce1ff302bf7714e8b28897355d010733
carlos-ios-developer <dr.carlos.iosdeveloper@gmail.com>
2018-12-31 14:57:11 +0200

    Initial commit

--- MovieDB/model/DetailsModel.swift
+++ MovieDB/model/DetailsModel.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class DetailModel: NSObject {
+class DetailsModel: NSObject {
 
-   
+    //MARK: get selected artist details
+    class func getDetails(artistId: Int){
+        let urlString = BEServant.Details(ID: artistId)
+        Alamofire.request(urlString).responseJSON { response in // method defaults to `.get`
+          
+            // Handling response
+            let result = JSON(response.result.value!)
+            let name = result["name"].string
+            let biography = result["biography"].string
+            let birthday = result["birthday"].string
+            UserDefaults.standard.set(name, forKey: "name")
+            UserDefaults.standard.set(biography, forKey: "biography")
+            UserDefaults.standard.set(birthday, forKey: "birthday")
+            
+            }
+        }
+    
+    
+    
+
+
 
 }
