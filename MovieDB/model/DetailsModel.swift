77c172f147bbad34026be571a6bc2fff1b462a1c
carlos-ios-developer <dr.carlos.iosdeveloper@gmail.com>
2018-12-31 13:57:28 +0200

    initial commit

---
+++ MovieDB/model/DetailsModel.swift
@@ -0,0 +1,15 @@
+//
+//  DeatilsViewControllerModel.swift
+//  MovieDB
+//
+//  Created by Carlos on 12/28/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class DetailModel: NSObject {
+
+   
+
+}
